3a3caa8f7b3623b4ddead3fea7baa043c9878b1d
wqrxysf <wqrxysf@yandex.ru>
2024-07-08 14:46:58 +0300

    renaming

--- OrderManager/OrderManager/Program.cs
+++ OrderManager/OrderManager/Program.cs
@@ -1,9 +1,9 @@
 ﻿Console.Write( "Введите название товара: " );
-string title = Console.ReadLine();
+string label = Console.ReadLine();
 
-Console.Write( $"Введите необходимое количество товара {title}: " );
-string amountTemplate = Console.ReadLine();
-int amount = CheckAmount( amountTemplate );
+Console.Write( $"Введите необходимое количество товара {label}: " );
+string tempQuantity = Console.ReadLine();
+int amount = CheckAmount( tempQuantity );
 
 Console.Write( "Введите Ваше имя: " );
 string name = Console.ReadLine();
@@ -11,25 +11,25 @@ string name = Console.ReadLine();
 Console.Write( $"{name}, введите адрес доставки: " );
 string address = Console.ReadLine();
 
-bool approve = ApproveOrder( title, amount, name, address );
+bool approve = ApproveOrder( label, amount, name, address );
 if ( approve )
 {
-    DateTime orderDay = CheckDays();
-    Console.WriteLine( $"{name}! Ваш заказ {title} в количестве {amount} оформлен! Ожидайте доставку по адресу {address} к {orderDay.ToLongDateString()}." );
+    DateTime orderDay = AddThreeDays();
+    Console.WriteLine( $"{name}! Ваш заказ {label} в количестве {amount} оформлен! Ожидайте доставку по адресу {address} к {orderDay.ToLongDateString()}." );
 }
 
 static int CheckAmount( string amount )
 {
-    while ( ( int.TryParse( amount, out int amo ) == false ) & ( amo <= 0 ) )
+    while ( ( int.TryParse( amount, out int amountTemplate ) == false ) && ( amountTemplate <= 0 ) )
     {
         Console.WriteLine( $"Введите корректное количество товара: " );
         amount = Console.ReadLine();
     }
-    int.TryParse( amount, out int amo2 );
-    return amo2;
+    int.TryParse( amount, out int amountCorrect );
+    return amountCorrect;
 }
 
-static DateTime CheckDays()
+static DateTime AddThreeDays()
 {
     DateTime nowDay = DateTime.Today;
     return nowDay.AddDays( 3 );
@@ -39,8 +39,8 @@ static bool ApproveOrder( string title, int amount, string name, string address 
 {
     Console.WriteLine( $"Здравствуйте, {name}, вы заказали {amount} {title} на адрес {address}, все верно?" );
     Console.Write( "Введите 'Да' для подтверждения заказа, и 'Нет' для его отмены: " );
-    string userData = Console.ReadLine();
-    if ( ( userData == "Да" ) | ( userData == "да" ) | ( userData == "'Да'" ) | ( userData == "'да'" ) )
+    string userData = Console.ReadLine().ToLower();
+    if ( ( userData == "да" ) || ( userData == "'да'" ) )
     {
         return true;
     }
@@ -48,8 +48,8 @@ static bool ApproveOrder( string title, int amount, string name, string address 
     {
         Console.WriteLine( $"Ваш ответ {userData} был распознан, как 'Нет'." );
         Console.Write( "Подтвердите отмену заказа, введя слово 'ОТМЕНА': " );
-        userData = Console.ReadLine();
-        if ( ( userData == "ОТМЕНА" ) | ( userData == "'ОТМЕНА'" ) | ( userData == "отмена" ) | ( userData == "'отмена'" ) )
+        userData = Console.ReadLine().ToLower();
+        if ( ( userData == "отмена" ) || ( userData == "'отмена'" ) )
         {
             return false;
         }
