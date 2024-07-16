4b4cbf32caaa4090fe80422796665499d06b53f9
wqrxysf <wqrxysf@yandex.ru>
2024-07-16 16:16:12 +0300

    внёс правки

--- OrderManager/OrderManager/Program.cs
+++ OrderManager/OrderManager/Program.cs
@@ -1,9 +1,9 @@
 ﻿Console.Write( "Введите название товара: " );
-string label = Console.ReadLine();
+string productName = Console.ReadLine();
 
-Console.Write( $"Введите необходимое количество товара {label}: " );
-string tempQuantity = Console.ReadLine();
-int amount = ValidateAmount( tempQuantity );
+Console.Write( $"Введите необходимое количество товара {productName}: " );
+string quantityString = Console.ReadLine();
+int amount = ValidateAmount( quantityString );
 
 Console.Write( "Введите Ваше имя: " );
 string name = Console.ReadLine();
@@ -11,16 +11,16 @@ string name = Console.ReadLine();
 Console.Write( $"{name}, введите адрес доставки: " );
 string address = Console.ReadLine();
 
-bool approve = ApproveOrder( label, amount, name, address );
-if ( approve )
+bool isApprove = ApproveOrder( productName, amount, name, address );
+if ( isApprove )
 {
     DateTime orderDay = AddThreeDays();
-    Console.WriteLine( $"{name}! Ваш заказ {label} в количестве {amount} оформлен! Ожидайте доставку по адресу {address} к {orderDay.ToLongDateString()}." );
+    Console.WriteLine( $"{name}! Ваш заказ {productName} в количестве {amount} оформлен! Ожидайте доставку по адресу {address} к {orderDay.ToLongDateString()}." );
 }
 
 static int ValidateAmount( string amount )
 {
-    while ( ( int.TryParse( amount, out int amountTemplate ) == false ) && ( amountTemplate <= 0 ) )
+    while ( ( int.TryParse( amount, out int amountTemplate ) == false ) )
     {
         Console.WriteLine( $"Введите корректное количество товара: " );
         amount = Console.ReadLine();
@@ -38,9 +38,11 @@ static DateTime AddThreeDays()
 static bool ApproveOrder( string title, int amount, string name, string address )
 {
     Console.WriteLine( $"Здравствуйте, {name}, вы заказали {amount} {title} на адрес {address}, все верно?" );
+
     Console.Write( "[1] - Подтвердить заказ" );
     Console.WriteLine( "[2] - Отменить заказ" );
     string userData = Console.ReadLine();
+
     while ( ( userData != "1" ) || ( userData != "2" ) )
     {
         Console.Write( "[1] - Подтвердить заказ" );
@@ -51,8 +53,5 @@ static bool ApproveOrder( string title, int amount, string name, string address 
     {
         return true;
     }
-    else
-    {
-        return false;
-    }
+    return false;
 }
